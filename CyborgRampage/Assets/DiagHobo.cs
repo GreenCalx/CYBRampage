cf10aec7ac2186eef2ab31d276fe07166758682e
soulkos <soulkos.aussie@gmail.com>
2016-10-17 22:48:57 +0200

    Added proximity detection for npc dialog
    creation of the proximity subroutine for npc dialog interractions

---
+++ CyborgRampage/Assets/DiagHobo.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DiagHobo : MonoBehaviour {
+
+    public int DialogFlag;
+    public int ProximityFlag; 
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.name.Equals("PLAYER"))
+        {
+            // we near enought of the madafaka
+            ProximityFlag = 1;            
+        }
+    }
+    void OnTriggerStay2D(Collider2D c)
+    {
+        if (c.gameObject.name.Equals("PLAYER"))
+        {
+            // we near enought of the madafaka
+            ProximityFlag = 1;
+        }
+    }
+    void OnTriggerExit2D(Collider2D c)
+    {
+        if (c.gameObject.name.Equals("PLAYER"))
+        {
+            // we going to far of the madafaka
+            ProximityFlag = 0;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        DialogFlag = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (ProximityFlag == 1)
+            {
+                // we can now speak to the madafaka
+                DialogFlag += 1;
+            }
+        }
+    }
+}
---
+++ CyborgRampage/Assets/Scripts/text_to_json.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class text_to_json : MonoBehaviour {
+
+    // Members
+
+    public int text_id;
+    public string text_class;
+    public string text_zone;
+    public string text_content;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
